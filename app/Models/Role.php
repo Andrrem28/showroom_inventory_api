c5f02f7f6532c6dd062072d5bf94da968b2119ef
André Martins <andree.martins_@hotmail.com>
2026-08-21 11:18:51 -0300

    feat: adiciona modelo Permission e atualiza modelo Role para gerenciamento de permissões

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+
+
     protected $fillable = [
         'name',
         'description',
@@ -16,8 +18,8 @@ class Role extends Model
     //     return $this->belongsToMany(User::class, 'role_user');
     // }
 
-    // public function permissions()
-    // {
-    //     return $this->belongsToMany(Permission::class, 'permission_role');
-    // }
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'permission_role');
+    }
 }
