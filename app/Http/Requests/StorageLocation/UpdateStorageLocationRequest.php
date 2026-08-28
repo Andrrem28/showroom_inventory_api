2b042ec4908ed17e8c0d2bdb3633d6865c0c1c38
André Martins <andree.martins_@hotmail.com>
2026-08-27 23:27:37 -0300

    feat: adiciona requisições para armazenamento de locais de armazenamento

---
+++ app/Http/Requests/StorageLocation/UpdateStorageLocationRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\StorageLocation;
+
+use App\DTOs\StorageLocationDTO;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateStorageLocationRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'        => 'sometimes|string|max:100',
+            'description' => 'nullable|string|max:255',
+        ];
+    }
+
+    public function toDTO(): StorageLocationDTO
+    {
+        return StorageLocationDTO::fromRequest($this->validated());
+    }
+}
